7b26b5089e5b5759762f11671e0fdb59ae089cbb
ald2424 <abigaild2424@gmail.com>
2020-03-24 20:29:43 -0400

    Added onclick events for log in forms

---
+++ project3/src/pages/InstructorLogin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Form(){
+    return(
+        <div>
+            <h1>Instructor Log In Page</h1>
+        </div>
+    )
+}
+
+export default Form;
--- project3/src/pages/Login.js
+++ project3/src/pages/Login.js
@@ -1,7 +1,19 @@
 import React from "react";
 import "../App.css"
+import history from "../utils/history"
 
 function Login(){
+
+    function openInstructorForm() {
+        history.push('/instructorform');
+        window.location.reload(false);
+      }
+    
+    function openStudentForm() {
+        history.push('/studentform');
+        window.location.reload(false);
+      }
+
     return(
         <div className="container text-center mx-auto my-5">
             <div className="row mb-5">
@@ -9,14 +21,14 @@ function Login(){
                     <div className="card-body">
                         <h2>Instructor Log In</h2>
                             <br></br>
-                        <button type="button" class="btn btn-danger btn-lg">Sign in Here</button>
+                        <button type="button" className="btn btn-danger btn-lg" onClick={openInstructorForm}>Sign in Here</button>
                     </div>
                 </div>
                 <div className="col-sm-6">
                     <div className="card-body">
                         <h2>Student Log In</h2>
                             <br></br>
-                        <button type="button" class="btn btn-danger btn-lg">Sign in Here</button>
+                        <button type="button" className="btn btn-danger btn-lg" onClick={openStudentForm}>Sign in Here</button>
                     </div>
                 </div>
             </div>
