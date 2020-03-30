33f98ed9e78e0a50663b90f45d136c0cdef0d5d1
ald2424 <abigaild2424@gmail.com>
2020-03-29 20:06:18 -0400

    the form is working using the command npm run dev

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://<username>:<password>@students-myhsi.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://dbUser:dbUserPassword@shearosefarm-i9h5g.mongodb.net/test?retryWrites=true&w=majority",
     secretOrKey: "secret"
   };
