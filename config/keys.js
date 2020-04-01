198210770062d2f9376c616abe4bbefb8fc9a333
ald2424 <abigaild2424@gmail.com>
2020-03-31 20:14:41 -0400

    updating mongo uri

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://dbUser:dbUserPassword@shearosefarm-agok0.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://dbUser:dbUserPassword@shearosefarm-gsjx6.mongodb.net/test?retryWrites=true&w=majority",
     secretOrKey: "secret"
   };
