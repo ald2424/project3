3f36eb84f643904dbc8357ef554a7a1f009c0a92
Greg Roberts <groberts3104@gmail.com>
2020-04-01 20:44:02 -0400

    none

--- client/src/components/dashboard/StudentDashboard.js
+++ client/src/components/dashboard/StudentDashboard.js
@@ -3,41 +3,31 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 import "./dashboard.css";
-import StudentNavBar from "../StudentNavBar";
-import Calendar from "../Calendar";
 
 class StudentDashboard extends Component {
   onLogoutClick = e => {
     e.preventDefault();
     this.props.logoutUser();
   };
-
+  
   render() {
     const { user } = this.props.auth;
-
     return (
-      <div>
-        <StudentNavBar />
-        <h1>Hey there,</h1> {user.name.split(" ")[0]}
-        <div className="col s12 center-align">
-          <Calendar />
-        </div>
-        <div style={{ height: "75vh" }} className="container valign-wrapper">
-          <div className="row">
-            <div className="col s12 center-align">
-              <button
-                style={{
-                  width: "150px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px",
-                  marginTop: "1rem"
-                }}
-                onClick={this.onLogoutClick}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-              >
-                Logout
-              </button>
-            </div>
+      <div style={{ height: "75vh" }} className="container mx-auto mt-5 pt-5">
+        <div className="row">
+          <div className="col s12 center-align">
+            <h4 className="font">Welcome {user.name.split(" ")[0]}!</h4>
+               <ul className="nav nav-tabs">
+                <li className="nav-item">
+                  <a className="nav-link text-dark active" href="#">Announcements</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-dark" href="#">Schedule</a>
+                </li>
+                <li className="nav-item">
+                  <button className="nav-link text-dark btn-link" onClick={this.onLogoutClick}>Logout</button>
+                </li>
+              </ul>
           </div>
         </div>
       </div>
@@ -45,7 +35,7 @@ class StudentDashboard extends Component {
   }
 }
 
-StudentDashboard.propTypes = {
+  StudentDashboard.propTypes = {
   logoutUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired
 };
@@ -54,4 +44,7 @@ const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, { logoutUser })(StudentDashboard);
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(StudentDashboard);
